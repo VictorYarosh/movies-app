d1318091ffea5b8ced9c24e98b7f5c5b6b463f18
VictorYarosh <77171768+VictorYarosh@users.noreply.github.com>
2023-02-11 14:21:52 +0200

    styled color body

--- src/componets/containers/layout/layout.styled.tsx
+++ src/componets/containers/layout/layout.styled.tsx
@@ -3,10 +3,7 @@ import styled from "styled-components";
 export const LayoutStyled = styled.div`
   height: 100%;
   min-height: 100vh;
-  background: linear-gradient(
-      60deg,
-      rgba(256, 0, 0, 0.8),
-      rgba(251, 0, 0, 0) 83.71%
-    ),
-    linear-gradient(89deg, rgba(0, 255, 0, 0.8), rgba(0, 255, 0, 0) 70.71%);
+  background: linear-gradient(268deg, #5a4af4, rgba(255, 0, 0, 0) -0.29%),
+    linear-gradient(60deg, #121828, rgba(0, 255, 0, 0) 244.71%),
+    linear-gradient(131deg, #1ea5fc, rgba(0, 0, 255, 0) 122.71%);
 `;
--- src/componets/ui/global.styles.ts
+++ src/componets/ui/global.styles.ts
@@ -7,6 +7,8 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Poppins', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    background: linear-gradient(217deg, #6447b5, rgba(255, 0, 0, 0) 70.71%),
+    linear-gradient(127deg, #82e1ff, rgba(0, 255, 0, 0) 70.71%)
   }
   code {
     font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
